ec2090c6b4b1896f6c7e379fb1bf8ccab71bd6af
Song jae hyeok <wakij6587@naver.com>
2022-05-22 11:17:34 +0900

    Feat: 비동기 코드 추가

--- study/fn.js
+++ study/fn.js
@@ -1,5 +1,22 @@
+const name = 'song';
+
 const fn = {
   add: (a, b) => a + b,
+  getNameCallback: callback => {
+    setTimeout(() => {
+      callback(name);
+      // callback(() => {
+      //   throw new Error('test error');
+      // });
+    }, 2000);
+  },
+  getNamePromise: () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(name);
+      }, 2000);
+    });
+  },
 };
 
 export default fn;
--- study/fn.test.js
+++ study/fn.test.js
@@ -1,13 +1,33 @@
 import fn from './fn';
 
-test('1을 기대한다.', () => {
-  expect(1).toBe(1);
-});
-
 test('2+3은 5다.', () => {
   expect(fn.add(2, 3)).toBe(5);
 });
 
-test('3+3은 5다.', () => {
-  expect(fn.add(3, 3)).not.toBe(5);
+test('이름은 song - async', async () => {
+  // const name = await fn.getNamePromise();
+  // expect(name).toBe('song');
+  // or
+  await expect(fn.getNamePromise()).resolves.toBe('song');
 });
+
+// test('이름은 song - promise', () => {
+//   // resolves or rejects
+//   return expect(fn.getNamePromise()).resolves.toBe('song');
+//   // return fn.getNamePromise().then(name => {
+//   //   expect(name).toBe('song');
+//   // });
+// });
+
+// test('이름은 song - callback', done => {
+//   function testCallback(name) {
+//     try {
+//       expect(name).toBe('song');
+//       done();
+//     } catch (error) {
+//       console.error(error);
+//       done();
+//     }
+//   }
+//   fn.getNameCallback(testCallback);
+// });
